d92e6aedc5223dd96c0bc119123aae7333bea6c3
josephyim224 <33196044+josephyim224@users.noreply.github.com>
2021-12-01 00:48:42 +0800

    hw simple fsm

--- hw/hw.srcs/sources_1/new/top.v
+++ hw/hw.srcs/sources_1/new/top.v
@@ -6,14 +6,19 @@ input clk;
 input btnC; // as reset button
 input RsRx;
 output RsTx;
-output[0:15] led;
+output [0:15] led;
 
-reg r_Tx_DV = 0;
-wire w_Tx_Done;
-reg [7:0] r_Tx_Byte = 0;
 wire clk_100MHz;
 
-assign led[1] = RsTx;
+reg r_Tx_DV = 0;
+reg [7:0] r_Tx_Byte = 0;
+wire w_Tx_Done;
+
+wire w_Rx_Dv;
+wire [7:0] w_Rx_Byte;
+
+assign led[1] = RsTx != 1;
+assign led[2] = RsRx != 1;
 
 clk_wiz_0 CLKGEN(
     .clk_out1(clk_100MHz),
@@ -25,8 +30,8 @@ clk_wiz_0 CLKGEN(
 uart_rx UART_RX_INST(
     .i_Clock(clk_100MHz),
     .i_Rx_Serial(RsRx),
-    .o_Rx_DV(),
-    .o_Rx_Byte()
+    .o_Rx_DV(w_Rx_Dv),
+    .o_Rx_Byte(w_Rx_Byte)
 );
 
 uart_tx UART_TX_INST(
@@ -43,12 +48,74 @@ breathing_led BREATHING_LED(
     .led(led[0])
 );
 
-reg[0:23] counter = 0;
+// controller
+parameter s_IDLE = 3'b000;
+parameter s_SET_SCALE_FACTOR = 3'b001;
+parameter s_SET_WRITE_ROW_NUMBER = 3'b010;
+parameter s_SET_WRITE_ROW = 3'b011;
+parameter s_PROCESS = 3'b100;
+parameter s_SET_SEND_ROW_NUMBER = 3'b101;
+parameter s_SEND_ROW  = 3'b110;
+
+reg [0:2] r_SM_Main = 0;
+reg [0:1] r_Scale_Factor = 0;
+reg [0:7] r_Write_Y = 0;
+reg [0:7] r_Read_Y = 0;
+
+assign led[3] = r_SM_Main == s_IDLE;
+assign led[4] = r_SM_Main == s_SET_SCALE_FACTOR;
+assign led[5] = r_SM_Main == s_SET_WRITE_ROW_NUMBER;
+assign led[6] = r_SM_Main == s_SET_WRITE_ROW;
+assign led[7] = r_SM_Main == s_PROCESS;
+assign led[8] = r_SM_Main == s_SET_SEND_ROW_NUMBER;
+assign led[9] = r_SM_Main == s_SEND_ROW;
+
 always @(posedge clk_100MHz) begin
-    counter <= counter+1;
-    if (counter == 10) r_Tx_Byte <= 0'b1010_1010;
-    else if (counter == 11)r_Tx_DV <= 1'b1;
-    else r_Tx_DV <= 1'b0;
+    case (r_SM_Main)
+        s_IDLE:
+            begin
+                if (w_Rx_Dv)
+                    case (w_Rx_Byte)
+                        8'd1: r_SM_Main <= s_SET_SCALE_FACTOR;
+                        8'd2: r_SM_Main <= s_SET_WRITE_ROW_NUMBER;
+                        8'd3: r_SM_Main <= s_PROCESS;
+                        8'd4: r_SM_Main <= s_SET_SEND_ROW_NUMBER;
+                    endcase
+             end
+        s_SET_SCALE_FACTOR:
+            begin
+                if (w_Rx_Dv)
+                    begin
+                        r_Scale_Factor <= w_Rx_Byte[1:0];
+                        r_SM_Main <= s_IDLE;
+                    end
+            end
+        s_SET_WRITE_ROW_NUMBER:
+            begin
+                if (w_Rx_Dv)
+                    begin
+                        r_Write_Y <= w_Rx_Byte;
+                        r_SM_Main <= s_SET_WRITE_ROW;
+                    end
+            end
+    //    s_SET_WRITE_ROW:
+    //         begin
+    //         end
+    //    s_PROCESS:
+    //         begin
+    //         end
+        s_SET_SEND_ROW_NUMBER:
+            begin
+                if (w_Rx_Dv)
+                    begin
+                        r_Read_Y <= w_Rx_Byte;
+                        r_SM_Main <= s_SEND_ROW;
+                    end
+            end
+        // s_SEND_ROW:
+        //     begin
+        //     end
+    endcase
 end
 
 endmodule
